4f65cc1c97dd9439ce48a627e55aa372951b78bb
allansrc <allanramos.teixeira@gmail.com>
2021-07-28 13:20:18 -0300

    update docs

--- lib/customPainters.dart
+++ lib/customPainters.dart
@@ -6,20 +6,14 @@ class OurPainter extends CustomPainter {
     /// [ Demo Line render ]
     var line = Paint()
       ..strokeWidth = 5
-      ..color = Colors.teal
+      ..color = Colors.black
       ..strokeCap = StrokeCap.round
-      ..style = PaintingStyle.stroke; // [needed by using path]
+      ..style = PaintingStyle.stroke;
 
     Offset startingPoint = Offset(0, size.height / 2);
     Offset endingPoint = Offset(size.width, size.height / 2);
 
     canvas.drawLine(startingPoint, endingPoint, line);
-
-    /// [ Demo line using path ]
-    var path = Path();
-    path.moveTo(0, size.height / 3);
-    path.lineTo(size.width, size.height / 3);
-    canvas.drawPath(path, line);
 
     /// [ Demo cricle ]
     var circle = Paint()
@@ -30,9 +24,66 @@ class OurPainter extends CustomPainter {
 
     Offset center = Offset(size.width / 2, size.height / 2);
 
-    canvas.drawCircle(center, 100, circle);
+    canvas.drawCircle(center, 200, circle);
 
-    /// [ Demo single point render ]
+    /// [ Demo  forns from rect ]
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+        Rect.fromLTWH(20, 35, 100, 100),
+        Radius.circular(20),
+      ),
+      line,
+    );
+
+    /// [ Demo oval ]
+    canvas.drawOval(
+      Rect.fromLTWH(20, 35, 100, 100),
+      line,
+    );
+
+    /// [ Demo path ]
+    var circleLine = Paint()
+      ..strokeWidth = 5
+      ..color = Colors.red
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+
+    var pathCircle = Path();
+
+    pathCircle.addOval(Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: 100,
+    ));
+
+    canvas.drawPath(pathCircle, circleLine);
+
+    /// [ Demo line using path ]
+    var pathLine = Path();
+
+    pathLine.moveTo(0, size.height / 3);
+    pathLine.lineTo(size.width, size.height / 3);
+
+    canvas.drawPath(pathLine, line);
+
+    /// [Demo arch path]
+    final arch = Path();
+    arch.moveTo(173, 142);
+    arch.arcToPoint(
+      Offset(size.width * 0.2, size.height * 0.6),
+      radius: Radius.circular(150),
+    );
+    // arch.arcToPoint(
+    //   Offset(size.width * 0.8, size.height * 0.6),
+    //   radius: Radius.circular(170),
+    //   clockwise: false,
+    // );
+    // arch.arcToPoint(
+    //   Offset(size.width * .65, 142),
+    //   radius: Radius.circular(170),
+    //   clockwise: true,
+    // );
+
+    canvas.drawPath(arch, line);
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -42,66 +42,6 @@ class ThePainter extends CustomPainter {
     Offset endingPoint = Offset(size.width, size.height / 2);
 
     canvas.drawLine(startingPoint, endingPoint, line);
-
-    var pathLine = Path();
-    pathLine.moveTo(0, size.height / 3);
-    pathLine.lineTo(size.width, size.height / 3);
-    canvas.drawPath(pathLine, line);
-
-    var circle = Paint()
-      ..strokeWidth = 5
-      ..color = Colors.blue
-      ..strokeCap = StrokeCap.round
-      ..style = PaintingStyle.stroke;
-
-    Offset center = Offset(size.width / 2, size.height / 2);
-
-    canvas.drawCircle(center, 200, circle);
-
-    canvas.drawRRect(
-      RRect.fromRectAndRadius(
-        Rect.fromLTWH(20, 35, 100, 100),
-        Radius.circular(20),
-      ),
-      line,
-    );
-
-    final arch = Path();
-    arch.moveTo(173, 142);
-    arch.arcToPoint(
-      Offset(size.width * 0.2, size.height * 0.6),
-      radius: Radius.circular(150),
-    );
-    arch.arcToPoint(
-      Offset(size.width * 0.8, size.height * 0.6),
-      radius: Radius.circular(170),
-      clockwise: false,
-    );
-    arch.arcToPoint(
-      Offset(size.width * .65, 142),
-      radius: Radius.circular(170),
-      clockwise: true,
-    );
-
-    canvas.drawPath(arch, line);
-
-    canvas.drawOval(
-      Rect.fromLTWH(20, 35, 100, 100),
-      line,
-    );
-
-    var circleLine = Paint()
-      ..strokeWidth = 5
-      ..color = Colors.red
-      ..strokeCap = StrokeCap.round
-      ..style = PaintingStyle.stroke;
-
-    var pathCircle = Path();
-    pathCircle.addOval(Rect.fromCircle(
-      center: Offset(size.width / 2, size.height / 2),
-      radius: 100,
-    ));
-    canvas.drawPath(pathCircle, circleLine);
   }
 
   @override
